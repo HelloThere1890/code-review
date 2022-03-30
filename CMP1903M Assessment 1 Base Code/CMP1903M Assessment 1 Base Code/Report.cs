001531857d55e6daf942ac46933ea66a0a76269a
HelloThere1890 <100799742+HelloThere1890@users.noreply.github.com>
2022-03-30 19:29:22 +0100

    Revert "Development"

--- CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Report.cs
+++ CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Report.cs
@@ -10,15 +10,7 @@ namespace CMP1903M_Assessment_1_Base_Code
     {
         //Handles the reporting of the analysis
         //Maybe have different methods for different formats of output?
-        //eg.   public void outputConsole(List<int>
+        //eg.   public void outputConsole(List<int>)
 
-        public void outputResults(List<int>parameters)
-        {
-            Console.WriteLine("sentences: {0}", parameters[0]);
-            Console.WriteLine("vowels: {0}", parameters[1]);
-            Console.WriteLine("consonants: {0}", parameters[2]);
-            Console.WriteLine("lower case: {0}", parameters[3]);
-            Console.WriteLine("upper case: {0}", parameters[4]);
-        }
     }
 }

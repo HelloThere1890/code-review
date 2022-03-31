cb9bd1b3fead173bc58704d73c6027e3c7893690
Mollie <molliesmy2002@gmail.com>
2022-03-31 02:32:26 +0100

    report

--- CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Report.cs
+++ CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Report.cs
@@ -10,7 +10,15 @@ namespace CMP1903M_Assessment_1_Base_Code
     {
         //Handles the reporting of the analysis
         //Maybe have different methods for different formats of output?
-        //eg.   public void outputConsole(List<int>)
+        //eg.   public void outputConsole(List<int>
 
+        public void outputResults(List<int> parameters)
+        {
+            Console.WriteLine("Number of sentences: {0}", parameters[0]);
+            Console.WriteLine("Number of vowels: {0}", parameters[1]);
+            Console.WriteLine("Number of consonants: {0}", parameters[2]);
+            Console.WriteLine("Number of lower case characters: {0}", parameters[3]);
+            Console.WriteLine("Number of upper case characters: {0}", parameters[4]);
+        }
     }
 }
